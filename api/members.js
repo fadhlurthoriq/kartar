3d9eed5c9c154232b3fea5c3a4fd0ad4c6ee7167
fadhlur28 <muhammadfthoriq@gmail.com>
2025-09-12 19:06:43 +0700

    update halaman index dengan card anggota

--- api/members.js
+++ api/members.js
@@ -3,20 +3,88 @@ export default function handler(req, res) {
   const data = [
     {
       id: 1,
-      name: "Satria Anugrah P",
+      name: "Zivanna Yunita A.",
       role: "Ketua",
-      profilePic: "img/koceng.jpeg",
+      profilePic: "",
       birthDate: "15 Januari 2005",
       address: "Jl. Merdeka No. 123, Jakarta",
       birthday: "15 Januari",
       photos: [
-        "img/koceng.jpeg",
-        "img/koceng.jpeg",
-        "img/koceng.jpeg",
-        "img/koceng.jpeg",
-        "img/koceng.jpeg"
+        "",
+        "",
+        "",
+        "",
+        ""
       ]
-    }
+    },
+
+    {
+      id: 2,
+      name: "Violla shevia a.r.",
+      role: "Wakil Ketua",
+      profilePic: "",
+      birthDate: "15 Januari 2005",
+      address: "Jl. Merdeka No. 123, Jakarta",
+      birthday: "15 Januari",
+      photos: [
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    
+    {
+      id: 3,
+      name: "Keysha Azzahra U",
+      role: "Sekretaris",
+      profilePic: "",
+      birthDate: "15 Januari 2005",
+      address: "Jl. Merdeka No. 123, Jakarta",
+      birthday: "15 Januari",
+      photos: [
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+
+    {
+      id: 4,
+      name: "Abelfi Lovely S.h.",
+      role: "Bendahara",
+      profilePic: "",
+      birthDate: "15 Januari 2005",
+      address: "Jl. Merdeka No. 123, Jakarta",
+      birthday: "15 Januari",
+      photos: [
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+
+    {
+      id: 5,
+      name: " Gilas Kusuma A.",
+      role: "Anggota",
+      profilePic: "",
+      birthDate: "15 Januari 2005",
+      address: "Jl. Merdeka No. 123, Jakarta",
+      birthday: "15 Januari",
+      photos: [
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
   ];
 
   res.status(200).json(data);
