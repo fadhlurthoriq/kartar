0abace7f6d5d49e412a342ee41527e3567e2fd53
fadhlur28 <muhammadfthoriq@gmail.com>
2025-09-12 13:17:33 +0700

    update nama anggota

--- api/members.js
+++ api/members.js
@@ -1,7 +1,7 @@
 // api/members.js
 export default function handler(req, res) {
   const data = [
-    { nama: "Budi", jabatan: "Ketua", avatar: "https://i.pravatar.cc/100?img=1" },
+    { nama: "Idub", jabatan: "Ketua", avatar: "https://i.pravatar.cc/100?img=1" },
     { nama: "Siti", jabatan: "Sekretaris", avatar: "https://i.pravatar.cc/100?img=2" },
     { nama: "Andi", jabatan: "Bendahara", avatar: "https://i.pravatar.cc/100?img=3" }
   ];
